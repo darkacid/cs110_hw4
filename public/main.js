620a46dcc787c4f42cf16a0e0a6d7527f9fed7f4
DarkAcid <sergstep314@gmail.com>
2016-10-31 23:38:41 +0400

    patch to undo the thingv2

--- public/main.js
+++ public/main.js
@@ -3,6 +3,9 @@ $(function()
   $("#searchBtn").on("click",function()
   {
       $.ajax({
-      })
-    });
+        url: "/public/index.html",
+        type: "POST",
+        contentType: "JSON",
+      });
+  });
 });
